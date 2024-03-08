a115f964b9fdf9de77c922eeafa50daa7f8a5fdf
omrkocar <omer@chasingratsgames.com>
2024-03-07 19:54:25 -0500

    EntitySystem now tracks Entities

--- Companions_Project/Assets/_Scripts/Silvermoon/Entity.cs
+++ Companions_Project/Assets/_Scripts/Silvermoon/Entity.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using Silvermoon.Core;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
+        EntitySystem.Track(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        EntitySystem.Untrack(this);
     }
 }
