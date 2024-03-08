7b5f458600faa6768be9eaff626c5f281b1133da
omrkocar <omer@chasingratsgames.com>
2024-03-07 19:37:33 -0500

    Created BaseSystem

---
+++ Companions_Project/Assets/_Scripts/Companions/Common/BaseSystem.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Silvermoon.Core;
+
+public abstract class BaseSystem : MonoBehaviour, ISystem
+{
+    protected virtual void Initialize() { }
+    protected virtual void Cleanup() { }
+
+    void ISystem.Initialize()
+    {
+        Initialize();
+    }
+
+    void ISystem.Cleanup()
+    {
+        Cleanup();
+    }
+}
+
+public abstract class BaseSystem<T> : BaseSystem where T : BaseSystem
+{
+    public static T Instance { get; private set; }
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this as T;
+    }
+}
