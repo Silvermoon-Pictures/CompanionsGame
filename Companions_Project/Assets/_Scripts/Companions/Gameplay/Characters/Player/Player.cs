aed6a6b59712d3049f00c65346457b28f1ca359b
omrkocar <omer@chasingratsgames.com>
2024-03-11 22:21:31 -0400

    GameInputSystem now gets accessed in Initialize

--- Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Player/Player.cs
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Player/Player.cs
@@ -5,44 +5,27 @@ using Companions.Systems;
 using Silvermoon.Movement;
 using UnityEngine;
 
-public partial class Player : MonoBehaviour
 public partial class Player : MonoBehaviour, ICompanionComponent
 {
-    private InputComponent inputComponent;
     private MovementComponent movementComponent;
     private InputComponent inputComponent;
     private PlayerCamera camera;
     
-    private void Awake()
     private void OnEnable()
     {
-        inputComponent = GetComponentInChildren<InputComponent>();
         inputComponent = GetComponent<InputComponent>();
         camera = GetComponentInChildren<PlayerCamera>();
         SetupMovement();
     }
 
-    private void Start()
     void ICompanionComponent.Initialize()
     {
-        SetupMovement();
         GameInputSystem.onInteract += OnInteract;
-        Debug.Log($"Player is initialized");
     }
 
-    // TODO OK: Move this into a partial class called Player.Movement
-    private void SetupMovement()
     void ICompanionComponent.Cleanup()
     {
-        movementComponent = GetComponent<MovementComponent>();
-        List<State> states = new List<State>
-        {
-            new WalkingMoveState(movementComponent),
-            new IdleMoveState(movementComponent),
-        };
         GameInputSystem.onInteract -= OnInteract;
     }
 
-        movementComponent.Initialize(states);
-    }
 }
