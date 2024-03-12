50f24b1b26ee67f3e82244a3296fb2d339f9ed5b
omrkocar <omer@chasingratsgames.com>
2024-03-11 20:28:37 -0400

    Created ComponentSystem that tracks ICoreComponents

---
+++ Companions_Project/Assets/_Scripts/Silvermoon/ComponentSystem.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace Silvermoon.Core
+{
+    public static class ComponentSystem<T> where T : ICoreComponent
+    {
+        public static IEnumerable<T> Components
+        {
+            get
+            {
+                var components = ComponentSystem.GetAllComponents<T>();
+                foreach (var component in components)
+                    yield return component;
+            }
+        }
+    }
+    
+    [RequiredSystem]
+    public class ComponentSystem : BaseSystem<ComponentSystem>
+    {
+        private Dictionary<Type, HashSet<ICoreComponent>> componentMap = new();
+        private List<ICoreComponent> allComponents = new();
+
+        public static void TrackComponent(ICoreComponent component)
+        {
+            Type type = component.GetType();
+            Instance.allComponents.Add(component);
+
+            if (!Instance.componentMap.ContainsKey(type)) 
+                Instance.componentMap[type] = new HashSet<ICoreComponent>();
+            
+            if (type.IsInstanceOfType(component))
+            {
+                Instance.componentMap[type].Add(component);
+            }
+        }
+
+        public static void UntrackAll()
+        {
+            Instance.componentMap.Clear();
+        }
+        
+        public static IEnumerable<T> GetAllComponents<T>(bool includeInactive = false) where T : ICoreComponent 
+        {
+            Type type = typeof(T);
+            if (Instance == null)
+                yield break;
+
+            var components = Instance.componentMap;
+            foreach (T component in components[type])
+            {
+                Component comp = component as Component;
+                if (comp == null)
+                    throw new Exception($"ComponentSystem - Illegal action on {component}: Attempting to get a component that does not inherit from Component");
+                
+                if (includeInactive || comp.gameObject.activeInHierarchy)
+                    yield return component;
+            }
+        }
+    }
+}
+
