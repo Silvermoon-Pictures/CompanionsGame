8a6a080a96d091682f9d213485b22b58fdaeffea
omrkocar <omer@chasingratsgames.com>
2024-03-16 10:17:03 -0400

    Setup Scene Dependencies now destroys the existing objects and re-creates them

--- Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
+++ Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using Companions.Core;
 using UnityEngine;
 
 public static class SetupSceneDependencies
@@ -8,6 +9,13 @@ public static class SetupSceneDependencies
     {
         GameObject object1 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_CompanionsGame.prefab");
         GameObject object2 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_PlayerSpawnPosition.prefab");
+        var core = Object.FindObjectOfType<CompanionsGame>();
+        var spawnPositions = Object.FindObjectOfType<PlayerSpawnPositionProvider>();
+        if (core != null)
+            Object.DestroyImmediate(core.gameObject);
+        if (spawnPositions != null)
+            Object.DestroyImmediate(spawnPositions.gameObject);
+        
         PrefabUtility.InstantiatePrefab(object1);
         PrefabUtility.InstantiatePrefab(object2);
     }
