cd61c31e4f34a1f04403999246b782af00fde9a7
omrkocar <omer@chasingratsgames.com>
2024-03-15 22:05:12 -0400

    SetupSceneDependencies button now adds a SpawnPositionProvider gameobject to the scene

--- Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
+++ Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
@@ -7,6 +7,8 @@ public static class SetupSceneDependencies
     public static void AddObjects()
     {
         GameObject object1 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_CompanionsGame.prefab");
+        GameObject object2 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_PlayerSpawnPosition.prefab");
         PrefabUtility.InstantiatePrefab(object1);
+        PrefabUtility.InstantiatePrefab(object2);
     }
 }
