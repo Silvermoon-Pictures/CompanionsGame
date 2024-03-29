d4de98c02ca959eef352f0aee9d7c1fee35f69de
omrkocar <omer@chasingratsgames.com>
2024-03-29 00:30:36 -0400

    IdentifierEditorWindow is now functional

--- Companions_Project/Assets/_Scripts/Companions/Editor/IdentifierEditorWindow.cs
+++ Companions_Project/Assets/_Scripts/Companions/Editor/IdentifierEditorWindow.cs
@@ -8,10 +8,11 @@ public class IdentifierEditorWindow : EditorWindow
     private IdentifiersAsset identifierAsset;
     public List<IdentifierCategory> Categories => identifierAsset.categories;
     private Vector2 scrollPosition;
+    private Vector2 leftScrollPosition, rightScrollPosition;
     private int selectedTabIndex = 0;
     private string newCategoryName = "";
     
-    private static readonly string assetPath = "Assets/_Data/Generated/Identifiers.asset";
+    public static readonly string assetPath = "Assets/_Data/Generated/Identifiers.asset";
     
     [MenuItem("Companions/Identifier Editor")]
     public static void ShowWindow()
@@ -27,25 +28,109 @@ public class IdentifierEditorWindow : EditorWindow
             EditorGUILayout.HelpBox("IdentifierAsset not found or not set.", MessageType.Error);
             return;
         }
-
-        if (identifierAsset.categories.Count > 0)
-            DrawIdentifiers();
         
-        EditorGUILayout.Space();
+        DrawTopButtons();
         EditorGUILayout.BeginHorizontal();
-        newCategoryName = EditorGUILayout.TextField("New Category Name", newCategoryName);
-        if (GUILayout.Button("Add Category") && !string.IsNullOrWhiteSpace(newCategoryName))
-        {
-            Categories.Add(new IdentifierCategory { categoryName = newCategoryName });
-            newCategoryName = "";
-            selectedTabIndex = Categories.Count - 1;
-        }
+        DrawCategories();
+        GUILayout.Box("", GUILayout.Width(1), GUILayout.ExpandHeight(true));
+        DrawIdentifiers();
         EditorGUILayout.EndHorizontal();
     }
 
+    private void DrawCategories()
+    {
+        EditorGUILayout.BeginVertical(GUILayout.Width(150));
+        leftScrollPosition = EditorGUILayout.BeginScrollView(leftScrollPosition);
+
+        for (int i = 0; i < Categories.Count; i++)
+        {
+            Rect buttonRect = GUILayoutUtility.GetRect(new GUIContent(Categories[i].categoryName), "Button", GUILayout.ExpandWidth(true));
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 1 && buttonRect.Contains(Event.current.mousePosition))
+            {
+                Event.current.Use();
+                ShowContextMenu(i);
+            }
+            
+            if (GUI.Button(buttonRect, Categories[i].categoryName) && Event.current.button == 0)
+            {
+                selectedTabIndex = i;
+            }
+
+            GUI.Button(buttonRect, Categories[i].categoryName);
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        newCategoryName = EditorGUILayout.TextField(newCategoryName, GUILayout.Width(100));
+        if (GUILayout.Button("Add", GUILayout.Width(40)) && !string.IsNullOrWhiteSpace(newCategoryName))
+        {
+            Categories.Add(new IdentifierCategory() { categoryName = newCategoryName });
+            newCategoryName = ""; // Reset after adding
+            selectedTabIndex = Categories.Count - 1; // Switch to the newly added category
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndVertical();
+    }
+    
     private void DrawIdentifiers()
     {
+        EditorGUILayout.BeginVertical();
+        rightScrollPosition = EditorGUILayout.BeginScrollView(rightScrollPosition);
+
+        if (Categories.Count > 0 && selectedTabIndex < Categories.Count)
+        {
+            var selectedCategory = Categories[selectedTabIndex];
+
+            for (int i = 0; i < selectedCategory.identifiers.Count; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                selectedCategory.identifiers[i] = EditorGUILayout.TextField(selectedCategory.identifiers[i]);
+                if (GUILayout.Button("-", GUILayout.Width(20)))
+                {
+                    selectedCategory.identifiers.RemoveAt(i);
+                    break;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+            if (GUILayout.Button("Add Identifier"))
+            {
+                selectedCategory.identifiers.Add("");
+            }
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("Select a category or add a new one.", MessageType.Info);
+        }
+
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndVertical();
+    }
+    
+    private void ShowContextMenu(int categoryIndex)
+    {
+        GenericMenu menu = new GenericMenu();
+        menu.AddItem(new GUIContent("Delete Category"), false, () => DeleteCategory(categoryIndex));
+        menu.ShowAsContext();
+    }
+
+
+    private void DeleteCategory(int index)
+    {
+        if (index >= 0 && index < Categories.Count)
+        {
+            Categories.RemoveAt(index);
+            selectedTabIndex = Mathf.Clamp(selectedTabIndex, 0, Categories.Count - 1);
+        }
+    }
+
+    private void DrawTopButtons()
+    {
         EditorGUILayout.BeginHorizontal();
+        
         if (GUILayout.Button("Save"))
         {
             SaveIdentifiers();
@@ -57,37 +142,6 @@ public class IdentifierEditorWindow : EditorWindow
         }
 
         EditorGUILayout.EndHorizontal();
-
-        string[] tabs = new string[identifierAsset.categories.Count];
-
-
-        for (int i = 0; i < Categories.Count; i++)
-        {
-            tabs[i] = Categories[i].categoryName;
-        }
-
-        selectedTabIndex = GUILayout.Toolbar(selectedTabIndex, tabs);
-        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-        var selectedCategory = identifierAsset.categories[selectedTabIndex];
-        
-        for (int i = 0; i < selectedCategory.identifiers.Count; i++)
-        {
-            EditorGUILayout.BeginHorizontal();
-            selectedCategory.identifiers[i] = EditorGUILayout.TextField(selectedCategory.identifiers[i]);
-            if (GUILayout.Button("-", GUILayout.Width(20)))
-            {
-                selectedCategory.identifiers.RemoveAt(i);
-                break;
-            }
-            EditorGUILayout.EndHorizontal();
-        }
-        
-        if (GUILayout.Button("Add Identifier"))
-        {
-            selectedCategory.identifiers.Add("");
-        }
-        
-        EditorGUILayout.EndScrollView();
     }
 
     private void ClearIdentifiers()
