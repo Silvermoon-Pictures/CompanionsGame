0d416f490b8a67ef18fc7a64020f1a96ed34a77a
omrkocar <omer@chasingratsgames.com>
2024-04-24 20:55:02 -0400

    Created RequestMoveState

---
+++ Companions_Project/Assets/_Scripts/Silvermoon/Movement/RequestMoveState.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Silvermoon.Movement
+{
+    public class RequestMoveState : MoveState
+    {
+        private MovementRequest moveRequest;
+        private float totalTime;
+        private readonly NavMeshAgent navmeshAgent;
+
+        private NavMeshPath path = new();
+
+        protected override bool CanEnter(MovementContext context)
+        {
+            return !context.request?.Done ?? false;
+        }
+
+        public override bool CanExit(MovementContext context)
+        {
+            return context.request?.Done ?? true;
+        }
+
+        protected override void OnExit(MovementContext context)
+        {
+            base.OnExit(context);
+
+            totalTime = 0;
+        }
+
+        protected override void OnEnter(MovementContext context)
+        {
+            base.OnEnter(context);
+            totalTime = 0;
+
+            moveRequest = context.request;
+        }
+
+        protected override void Update(MovementContext context)
+        {
+            base.Update(context);
+
+            if (moveRequest != context.request)
+            {
+                moveRequest = context.request;
+                totalTime = 0f;
+            }
+
+            totalTime += context.dt;
+            var requestPosition = context.request.Evaluate(context, totalTime);
+            // context.velocity = (requestPosition - context.position) / context.dt;
+            // Debug.DrawLine(requestPosition, context.position, Color.red, .3f);
+            
+            navmeshAgent.CalculatePath(requestPosition, path);
+            navmeshAgent.SetPath(path);
+
+            context.velocity = navmeshAgent.velocity.normalized * context.speed;
+        }
+
+        protected override void PostUpdate(MovementContext context)
+        {
+            base.PostUpdate(context);
+
+            navmeshAgent.nextPosition = context.position;
+        }
+
+        public RequestMoveState(MovementComponent owner, NavMeshAgent navMeshAgent) : base(owner)
+        {
+            this.navmeshAgent = navMeshAgent;
+        }
+    }   
+}
