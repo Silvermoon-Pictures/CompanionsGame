4381e324cd4e3cd3d2fafa0f9284948cb0a85487
omrkocar <omer@chasingratsgames.com>
2024-05-02 14:42:13 -0400

    Buildings can now be upgraded infinitely

--- Companions_Project/Assets/_Scripts/Companions/Gameplay/LevelDesign/Building.cs
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/LevelDesign/Building.cs
@@ -1,12 +1,24 @@
+using System.Collections.Generic;
 using Silvermoon.Core;
 using UnityEngine;
 
 public class Building : MonoBehaviour, ICoreComponent
 {
-    public GameObject secondPhase;
+    public List<GameObject> phases = new();
 
+    private int currentPhase = 0;
+    
     public void Upgrade()
     {
-        secondPhase.SetActive(true);
+        if (currentPhase == phases.Count)
+            return;
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        
+        phases[currentPhase].SetActive(true);
+        currentPhase++;
     }
 }
