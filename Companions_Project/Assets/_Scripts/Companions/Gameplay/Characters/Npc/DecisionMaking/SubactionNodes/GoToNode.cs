4fd867d12fbbeaccf7ac8fb7b531a4316339fec1
omrkocar <omer@chasingratsgames.com>
2024-07-18 16:42:30 -0400

    Created LookAtNode

--- Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/SubactionNodes/GoToNode.cs
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/SubactionNodes/GoToNode.cs
@@ -5,25 +5,14 @@ using UnityEngine;
 public class GoToNode : SubactionNode
 {
     public float stoppingDistance = 1f;
-    public Identifier lookAtPoint;
     
     public override IEnumerator Execute(SubactionContext context)
     {
-        GameObject lookAtTarget = context.dictionaryComponent.Get<GameObject>(lookAtPoint);
-        if (lookAtTarget != null)
-        {
-            Vector3 direction = (lookAtTarget.transform.position - context.npc.transform.position).normalized;
-            if (direction != Vector3.zero)
-            {
-                Quaternion lookRotation = Quaternion.LookRotation(direction);
-                context.npc.transform.rotation = Quaternion.Slerp(context.npc.transform.rotation, lookRotation, Time.deltaTime * 10f);
-            }
-        }
-        
         float stoppingDistanceSqr = stoppingDistance * stoppingDistance + 1;
         while ((context.npc.transform.position - context.target.transform.position).sqrMagnitude > stoppingDistanceSqr)
         {
             context.npc.GoTo(context.target.transform.position, stoppingDistance);
+            
             yield return null;
         }
     }
---
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/SubactionNodes/LookAtNode.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+[ActionGraphContext("Look At")]
+public class LookAtNode : SubactionNode
+{
+    public Identifier lookAtPoint;
+    public float rotationSpeed = 0.5f;
+
+    public override IEnumerator Execute(SubactionContext context)
+    {
+        GameObject lookAtTarget = context.dictionaryComponent.Get<GameObject>(lookAtPoint);
+        if (lookAtTarget == null)
+            yield break;
+        
+        Vector3 direction = (lookAtTarget.transform.position - context.npc.transform.position).normalized;
+        while (true)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            float angle = Quaternion.Angle(context.npc.transform.rotation, lookRotation);
+            
+            if (angle < 5f)
+                break;
+            
+            context.npc.transform.rotation =
+                Quaternion.Slerp(context.npc.transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+            
+            yield return null;
+        }
+    }
+}
