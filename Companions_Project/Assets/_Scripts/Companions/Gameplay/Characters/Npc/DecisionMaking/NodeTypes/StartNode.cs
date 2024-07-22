7ff4db5524b563a7e18679ce4959fb9bf4c6323b
omrkocar <omer@chasingratsgames.com>
2024-07-22 10:11:58 -0400

    Removed redundant code in StartNode

--- Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/NodeTypes/StartNode.cs
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/NodeTypes/StartNode.cs
@@ -1,12 +1,4 @@
-using System.Collections;
-using UnityEngine;
-
 [NodeInfo("Start", "Events/Start", hasInput: false, hasOutput: true)]
 public class StartNode : ActionGraphNode
 {
-    public override IEnumerator Execute(SubactionContext context)
-    {
-        Debug.Log("Start Node!");
-        yield break;
-    }
 }
