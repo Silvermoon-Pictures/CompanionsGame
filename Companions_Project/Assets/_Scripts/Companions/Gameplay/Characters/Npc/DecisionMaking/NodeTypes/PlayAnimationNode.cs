e3cb9f04a6a24dfef01a9c88565184f44556e683
omrkocar <omer@chasingratsgames.com>
2024-07-22 08:30:08 -0400

    Added waiting for transition in AnimationNode

--- Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/NodeTypes/PlayAnimationNode.cs
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/NodeTypes/PlayAnimationNode.cs
@@ -8,6 +8,7 @@ public class PlayAnimationNode : ActionGraphNode
     public string booleanName;
     public bool boolean;
     public string triggerName;
+    public bool waitForTransition;
 
     public override IEnumerator Execute(SubactionContext context)
     {
@@ -19,6 +20,13 @@ public class PlayAnimationNode : ActionGraphNode
             animator.SetBool(booleanName, boolean);
         if (!string.IsNullOrEmpty(triggerName))
             animator.SetTrigger(triggerName);
+
+        if (waitForTransition)
+        {
+            yield return new WaitUntil(() => animator.IsInTransition(0));
+            var transitionInfo = animator.GetAnimatorTransitionInfo(0);
+            yield return new WaitForSeconds(transitionInfo.duration);
+        }
         
         if (clip != null)
             yield return new WaitForSeconds(clip.length);
