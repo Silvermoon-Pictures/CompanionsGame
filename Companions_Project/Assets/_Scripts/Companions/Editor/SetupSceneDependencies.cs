4feb11a3917da8fe4f70f53eac22cb7527bcd083
omrkocar <omer@chasingratsgames.com>
2024-07-27 10:16:14 -0400

    Removed camera from SetupSceneDependencies

--- Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
+++ Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
@@ -9,19 +9,14 @@ public static class SetupSceneDependencies
     {
         GameObject object1 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_CompanionsGame.prefab");
         GameObject object2 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_PlayerSpawnPosition.prefab");
-        GameObject object3 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_MainCamera.prefab");
         var core = Object.FindObjectOfType<CompanionsGame>();
         var spawnPositions = Object.FindObjectOfType<PlayerSpawnPositionProvider>();
-        var mainCamera = Object.FindObjectOfType<Camera>();
         if (core != null)
             Object.DestroyImmediate(core.gameObject);
         if (spawnPositions != null)
             Object.DestroyImmediate(spawnPositions.gameObject);
-        if (mainCamera != null)
-            Object.DestroyImmediate(spawnPositions.gameObject);
         
         PrefabUtility.InstantiatePrefab(object1);
         PrefabUtility.InstantiatePrefab(object2);
-        PrefabUtility.InstantiatePrefab(object3);
     }
 }
