d7229f1ac1fa3cee3c351ae4b2fc0baf863c23d8
omrkocar <omer@chasingratsgames.com>
2024-07-31 08:33:46 -0400

    videos can now be played through cutscene component

--- Companions_Project/Assets/_Scripts/Companions/Systems/CutsceneSystem.cs
+++ Companions_Project/Assets/_Scripts/Companions/Systems/CutsceneSystem.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using Silvermoon.Core;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Video;
 
 [RequiredSystem]
 public class CutsceneSystem : BaseSystem<CutsceneSystem>
@@ -21,25 +22,32 @@ public class CutsceneSystem : BaseSystem<CutsceneSystem>
     }
 
     private event EventHandler cutsceneStopped;
-    
-    protected override void Initialize(GameContext context)
-    {
-        base.Initialize(context);
-        
-        
-    }
 
     public static void Play(PlayableDirector director)
     {
-        Instance.cutsceneStarted?.Invoke(director, EventArgs.Empty);
+        Instance.cutsceneStarted?.Invoke(Instance, EventArgs.Empty);
         director.Play();
         director.stopped += OnCutsceneEnded;
+    }
+    
+    public static void Play(VideoPlayer videoPlayer)
+    {
+        Instance.cutsceneStarted?.Invoke(Instance, EventArgs.Empty);
+        videoPlayer.Play();
+        videoPlayer.loopPointReached += OnVideoEnded;
     }
 
     private static void OnCutsceneEnded(PlayableDirector director)
     {
         director.stopped -= OnCutsceneEnded;
         Instance.cutsceneStopped?.Invoke(director, EventArgs.Empty);
+
+    }
+    
+    private static void OnVideoEnded(VideoPlayer videoPlayer)
+    {
+        videoPlayer.loopPointReached -= OnVideoEnded;
+        Instance.cutsceneStopped?.Invoke(Instance, EventArgs.Empty);
 
     }
 }
