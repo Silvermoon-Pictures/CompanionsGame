1d7451172fcbc807f2329b3249dad523a2a61abd
omrkocar <kocartahir@gmail.com>
2024-08-04 20:40:29 -0400

    Created ScriptedActionAsset and a base class for this and ActionAsset

--- Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/ActionAsset.cs
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/ActionAsset.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 [CreateAssetMenu(menuName = "Companions/Npc/Actions", fileName = "Action")]
-public class ActionAsset : SerializedScriptableObject
+public class ActionAsset : BaseAction
 {
     [SerializeField]
     private float cooldown = 0f;
@@ -24,62 +24,7 @@ public class ActionAsset : SerializedScriptableObject
     [TitleGroup("Decision Making")]
     public List<Consideration> incompatibleConsiderations = new();
 
-
-    [SerializeReference, HideInInspector]
-    private List<ActionGraphNode> graphNodes;
-    [SerializeField, HideInInspector]
-    private List<ActionGraphConnection> connections;
-
-    public List<ActionGraphNode> GraphNodes => graphNodes;
-    public List<ActionGraphConnection> Connections => connections;
-    public Queue<ActionGraphNode> SubactionQueue { get; private set; } = new();
-    public Queue<ActionGraphNode> UpdateSubactionQueue { get; private set; } = new();
-    public Queue<ActionGraphNode> ExitSubactionQueue { get; private set; } = new();
-    public Queue<ActionGraphNode> InitializeSubactionQueue { get; private set; } = new();
-
-    public ActionAsset()
-    {
-        graphNodes = new List<ActionGraphNode>();
-        connections = new List<ActionGraphConnection>();
-    }
-
-
-    private void OnEnable()
-    {
-        Init();
-        FillSubactions();
-    }
-
-    void FillSubactions()
-    {
-        ActionGraphNode currentNode = GetNode(GetInitializeNode()?.NextNodeId);
-        while (currentNode != null)
-        {
-            InitializeSubactionQueue.Enqueue(currentNode);
-            currentNode = GetNode(currentNode.NextNodeId);
-        }
-
-        currentNode = GetNode(GetStartNode()?.NextNodeId);
-        while (currentNode != null)
-        {
-            SubactionQueue.Enqueue(currentNode);
-            currentNode = GetNode(currentNode.NextNodeId);
-        }
-        
-        currentNode = GetNode(GetUpdateNode()?.NextNodeId);
-        while (currentNode != null)
-        {
-            UpdateSubactionQueue.Enqueue(currentNode);
-            currentNode = GetNode(currentNode.NextNodeId);
-        }
-        
-        currentNode = GetNode(GetExitNode()?.NextNodeId);
-        while (currentNode != null)
-        {
-            ExitSubactionQueue.Enqueue(currentNode);
-            currentNode = GetNode(currentNode.NextNodeId);
-        }
-    }
+    
     
     public float CalculateScore(ConsiderationContext context)
     {
@@ -150,63 +95,7 @@ public class ActionAsset : SerializedScriptableObject
         return true;
     }
 
-    private ActionGraphNode GetStartNode()
-    {
-        ExecuteNode[] startNodes = graphNodes.OfType<ExecuteNode>().ToArray();
-        if (startNodes.Length == 0)
-        {
-            return null;
-        }
-        return startNodes[0];
-    }
     
-    public ActionGraphNode GetUpdateNode()
-    {
-        UpdateNode[] updateNodes = graphNodes.OfType<UpdateNode>().ToArray();
-        if (updateNodes.Length == 0)
-        {
-            return null;
-        }
-        return updateNodes[0];
-    }
-    
-    private ActionGraphNode GetExitNode()
-    {
-        ExitNode[] exitNodes = graphNodes.OfType<ExitNode>().ToArray();
-        if (exitNodes.Length == 0)
-        {
-            return null;
-        }
-        return exitNodes[0];
-    }
-    
-    private ActionGraphNode GetInitializeNode()
-    {
-        InitializeNode[] initializeNode = graphNodes.OfType<InitializeNode>().ToArray();
-        if (initializeNode.Length == 0)
-        {
-            return null;
-        }
-        return initializeNode[0];
-    }
-
-    private ActionGraphNode GetNode(string nextNodeCurrent)
-    {
-        if (string.IsNullOrEmpty(nextNodeCurrent))
-            return null;
-        
-        return nodeDictionary.GetValueOrDefault(nextNodeCurrent);
-    }
-
-    private Dictionary<string, ActionGraphNode> nodeDictionary;
-    public void Init()
-    {
-        nodeDictionary = new();
-        foreach (var node in graphNodes)
-        {
-            nodeDictionary.Add(node.Id, node);
-        }
-    }
 }
 
 public enum ETargetType
---
+++ Companions_Project/Assets/_Scripts/Companions/Gameplay/Characters/Npc/DecisionMaking/ScriptedActionAsset.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Companions/Npc/Scripted Action", fileName = "ScriptedAction")]
+public class ScriptedActionAsset : BaseAction
+{
+    
+}
