104959d8d9ca9f912e6b47f6de69d79a2dcdfd85
omrkocar <kocartahir@gmail.com>
2024-09-13 19:59:55 -0400

    Scene dependencies are now added automatically on play button hit

--- Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
+++ Companions_Project/Assets/_Scripts/Companions/Editor/SetupSceneDependencies.cs
@@ -1,22 +1,62 @@
 using UnityEditor;
 using Companions.Core;
+using Unity.AI.Navigation;
 using UnityEngine;
 
-public static class SetupSceneDependencies
+namespace Companions.Editor
 {
-    [MenuItem("Companions/Setup Scene Dependencies")]
-    public static void AddObjects()
+    [InitializeOnLoad]
+    public class SetupSceneDependencies : EditorWindow
     {
-        GameObject object1 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_CompanionsGame.prefab");
-        GameObject object2 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_PlayerSpawnPosition.prefab");
-        var core = Object.FindObjectOfType<CompanionsGame>();
-        var spawnPositions = Object.FindObjectOfType<PlayerSpawnPositionProvider>();
-        if (core != null)
-            Object.DestroyImmediate(core.gameObject);
-        if (spawnPositions != null)
-            Object.DestroyImmediate(spawnPositions.gameObject);
-        
-        PrefabUtility.InstantiatePrefab(object1);
-        PrefabUtility.InstantiatePrefab(object2);
+        [MenuItem("Companions/Setup Scene Dependencies")]
+        public static void Method()
+        {
+            AddSceneDependencies();
+        }
+
+        private static void AddSceneDependencies()
+        {
+            GameObject object1 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_CompanionsGame.prefab");
+            GameObject object2 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_PlayerSpawnPosition.prefab");
+            GameObject object3 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Prefabs/Tech/GO_NavMeshSurface.prefab");
+
+            var game = FindObjectOfType<CompanionsGame>();
+            var spawnPositions = FindObjectOfType<PlayerSpawnPositionProvider>();
+            var navMeshSurface = FindObjectOfType<NavMeshSurface>();
+
+            if (game == null)
+                PrefabUtility.InstantiatePrefab(object1);
+            if (spawnPositions == null)
+                PrefabUtility.InstantiatePrefab(object2);
+            if (navMeshSurface == null)
+                PrefabUtility.InstantiatePrefab(object3);
+        }
+
+        private static bool HasSceneDependencies()
+        {
+            if (FindObjectOfType<CompanionsGame>() == null)
+                return false;
+            if (FindObjectOfType<PlayerSpawnPositionProvider>() == null)
+                return false;
+            if (FindObjectOfType<NavMeshSurface>() == null)
+                return false;
+
+            return true;
+        }
+
+        static SetupSceneDependencies()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeChanged;
+        }
+
+        private static void OnPlayModeChanged(PlayModeStateChange state)
+        {
+            if (state != PlayModeStateChange.ExitingEditMode) 
+                return;
+            if (HasSceneDependencies())
+                return;
+            
+            AddSceneDependencies();
+        }
     }
 }
