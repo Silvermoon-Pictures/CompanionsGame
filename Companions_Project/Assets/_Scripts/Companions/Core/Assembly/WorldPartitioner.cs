88acabbbed5cf576035b3176a8c6c9bdd5bdebf2
omrkocar <kocartahir@gmail.com>
2024-09-24 17:57:36 -0400

    World partitioner is now an editor window

---
+++ Companions_Project/Assets/_Scripts/Companions/Core/Assembly/WorldPartitioner.cs
@@ -0,0 +1,69 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WorldPartitionerWindow : EditorWindow
+{
+    private float activateDistance = 100f;
+
+    [MenuItem("Companions/World Partitioner")]
+    public static void ShowWindow()
+    {
+        GetWindow<WorldPartitionerWindow>("World Partitioner");
+    }
+
+    private void OnGUI()
+    {
+        activateDistance = EditorGUILayout.FloatField("Activate Distance", activateDistance);
+
+        if (GUILayout.Button("Activate Nearby Objects"))
+        {
+            ActivateNearbyObjects(activateDistance);
+        }
+
+        if (GUILayout.Button("Activate All Objects"))
+        {
+            SetActivateAllObjects(true);
+        }
+
+        if (GUILayout.Button("Disable All Objects"))
+        {
+            SetActivateAllObjects(false);
+        }
+    }
+    
+    private static IEnumerable<GameObject> GetObjects()
+    {
+        foreach (GameObject obj in FindObjectsOfType<GameObject>(true))
+        {
+            if (!obj.TryGetComponent(out LevelDesignComponent _))
+                continue;
+
+            yield return obj;
+        }
+    }
+    
+    private void ActivateNearbyObjects(float distance)
+    {
+        Vector3 cameraPosition = SceneView.lastActiveSceneView.camera.transform.position;
+        Vector2 cameraGridPosition = new Vector2(cameraPosition.x, cameraPosition.z);
+
+        foreach (var obj in GetObjects())
+        {
+            Vector3 objPosition = obj.transform.position;
+            Vector2 objGridPosition = new(objPosition.x, objPosition.z);
+            bool activate = Vector2.Distance(cameraGridPosition, objGridPosition) < distance;
+            obj.SetActive(activate);
+        }
+    }
+
+    public static void SetActivateAllObjects(bool activate)
+    {
+        foreach (var obj in GetObjects())
+        {
+            obj.SetActive(activate);
+        }
+    }
+}
+#endif
